cb7eafe4dafd3d08d189b0336ae7e697099fa095
savio <savioanjos2020@gmail.com>
2023-03-31 14:36:10 -0300

    Criando lógica para atualizar user

--- src/server.ts
+++ src/server.ts
@@ -86,7 +86,7 @@ const server = new ApolloServer({
                         email: args.email,
                         password: args.password
                     }
-                
+                 
             })
 
             return updatedUser;
