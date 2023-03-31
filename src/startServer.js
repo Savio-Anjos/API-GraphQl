2e93873f23fb019bc4b5b4a54f9ffba69f7c2b49
savio <savioanjos2020@gmail.com>
2023-03-30 21:27:58 -0300

    Limpando arquivos

--- src/startServer.js
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from "apollo-server";
-
-function startServer({ typeDefs, resolvers }) {
-  const server = new ApolloServer({ typeDefs, resolvers });
-
-  server.listen().then(({ url }) => console.log(`Server started at ${url}`));
-}
-
-export default startServer;
