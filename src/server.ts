2e6c2f097e81c4d30de1d2dd521eb524c6443507
savio <savioanjos2020@gmail.com>
2023-03-31 14:43:10 -0300

    Deletando diretórios desnecessários

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import { ApolloServer, gql } from 'apollo-server';
-import { randomUUID } from 'node:crypto';
-import prismaClient from './prisma';
+import { ApolloServer, gql } from "apollo-server";
+import { randomUUID } from "node:crypto";
+import prismaClient from "./prisma";
 
 /*
  * Under fetching
  * Rota HTTP que retorna dados de menos
- * 
+ *
  * Over fetching
  * Rota HTTP que retorna mais dados do que precisamos
-*/
+ */
 
 const typeDefs = gql`
   type User {
@@ -17,84 +17,75 @@ const typeDefs = gql`
     email: String!
     password: String!
   }
-  
+
   type Query {
+    # Listar usuários
     users: [User!]!
   }
- type Mutation {
+  type Mutation {
     # Cadastrar usuário
     createUser(name: String!, email: String!, password: String!): User!
 
-    # Deletar usuário
-    deleteUser(id: ID!): Boolean
-
     # Atualizar usuário
     updateUser(id: ID!, name: String!, email: String!, password: String!): User!
- }
-`
 
-interface User {
-    id: string
-    name: string
-    email: string
-    password: string
-}
-
+    # Deletar usuário
+    deleteUser(id: ID!): Boolean
+  }
+`;
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers: {
-        Query: {
-            users: () => {
-                return prismaClient.user.findMany()
-            }
-        },
+  typeDefs,
+  resolvers: {
+    Query: {
+      users: () => {
+        return prismaClient.user.findMany();
+      },
+    },
 
-        Mutation: {
+    Mutation: {
+      //Criar usuário
+      createUser: (_, args) => {
+        const user = prismaClient.user.create({
+          data: {
+            id: randomUUID(),
+            name: args.name,
+            email: args.email,
+            password: args.password,
+          },
+        });
 
-            //Criar usuário
-            createUser: (_, args) => {
-                const user = prismaClient.user.create({
-                    data: {
-                        id: randomUUID(),
-                        name: args.name,
-                        email: args.email,
-                        password: args.password
-                    }
-                })
+        return user;
+      },
 
-                return user;
-            },
+      //Deletar usuário
+      deleteUser: async (_, args) => {
+        const deletedUser = await prismaClient.user.delete({
+          where: {
+            id: args.id,
+          },
+        });
 
-            //Deletar usuário
-            deleteUser: async (_, args) => {
-                const deletedUser = await prismaClient.user.delete({
-                    where: {
-                        id: args.id
-                    }
-                })
+        return !!deletedUser;
+      },
 
-                return !!deletedUser;
-            },
+      //Atualizar usuário
+      updateUser: (_, args) => {
+        const updatedUser = prismaClient.user.update({
+          where: { id: args.id },
+          data: {
+            name: args.name,
+            email: args.email,
+            password: args.password,
+          },
+        });
 
-            //Atualizar usuário
-            updateUser: (_, args) => {
-                const updatedUser = prismaClient.user.update({
-                    where: { id: args.id },
-                    data: {
-                        name: args.name,
-                        email: args.email,
-                        password: args.password
-                    }
-                 
-            })
-
-            return updatedUser;
-        }
-    }
-    }
-})
+        return updatedUser;
+      },
+    },
+  },
+});
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 HTTP server running on ${url}`);
+  console.log(`🚀 HTTP server running on ${url}`);
 });
